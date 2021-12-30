7a95e0984ab5f5baad4430384bc08bbbf5109d72
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2021-12-30 15:21:06 +0100

    main(root): Added .gitattributes

--- yautpa-desktop/src/main.tsx
+++ yautpa-desktop/src/main.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { Switch, Route, BrowserRouter as Router} from "react-router-dom";
-import './index.scss'
-import './pages/Home';
 import Home from "./pages/Home";
+import './index.scss';
 
 ReactDOM.render(
     <React.StrictMode>
